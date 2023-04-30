d301c8ab04b6086120f95806f348b7c869009f6a
0urobor0s <0urobor0s@users.noreply.github.com>
2023-04-30 16:40:32 +0100

    Update honu

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule PhoenixHonuExample.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:gettext] ++ Mix.compilers(),
+      compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
@@ -49,7 +49,10 @@ defmodule PhoenixHonuExample.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:honu, "~> 0.1.0"}
+      {:honu, "~> 0.3.0"},
+      #{:honu, path: "../honu"},
+      {:aws, "~> 0.11"},
+      {:hackney, "~> 1.17"}
     ]
   end
 
